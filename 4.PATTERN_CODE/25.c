c3e93d49b2b257a3ec78dbc71b8293cacacff0ac
Om Bhandarkar <ombhandarkar1206@gmail.com>
2023-07-20 10:37:42 +0530

    update

---
+++ 4.PATTERN_CODE/25.c
@@ -0,0 +1,26 @@
+//                                  *     * * * * 
+//                                  *     *       
+//                                  *     *       
+//                                  * * * * * * * 
+//                                        *     * 
+//                                        *     * 
+//                                  * * * *     *
+
+
+#include<stdio.h>
+void main(){
+    int n = 7;
+    int mid = n / 2 + 1;
+
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            if(i==mid || j==mid || (i==1 && j>=mid) || (j==1 && i<=mid) || (i==n && j<=mid) || (j==n && i>=mid)){
+                printf("* ");
+            }else{
+                printf("  ");
+            }
+        }
+        printf("\n");
+    }
+    
+}
