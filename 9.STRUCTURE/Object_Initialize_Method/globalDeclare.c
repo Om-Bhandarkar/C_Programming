d0243fa5e21a5cc37345205332ae2fd9a0501b3f
ombhandarkar1206 <ombhandarkar1206@gmail.com>
2023-08-11 01:57:05 +0530

    update

--- 9.STRUCTURE/Object_Initialize_Method/globalDeclare.c
+++ 9.STRUCTURE/Object_Initialize_Method/globalDeclare.c
@@ -6,8 +6,15 @@ struct cricPlayer
     char pName[10];
     int jerNo;
     float avg;
-}obj1 = { };
+}obj1 = {"MSDhoni",07,45.48};
 
 int main(){
-
+    struct cricPlayer obj2 = {"Virat",18,50.33};
+    printf("%s\n",obj1.pName); 
+    printf("%d\n",obj1.jerNo); 
+    printf("%f\n",obj1.avg); 
+    printf("%s\n",obj2.pName); 
+    printf("%d\n",obj2.jerNo); 
+    printf("%f\n",obj2.avg); 
+    return 0;
 }
